2c2bd94e2b632d092a428fed9e3843f021eab900
Leonardo Cunha <cunhacardosoleo@gmail.com>
2021-09-17 23:58:42 -0300

    Initial Commit

---
+++ perguntasRespostas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  perguntasRespostas
+//
+//  Created by Leonardo Cunha on 17/09/21.
+//  Copyright © 2021 Leonardo Cunha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
