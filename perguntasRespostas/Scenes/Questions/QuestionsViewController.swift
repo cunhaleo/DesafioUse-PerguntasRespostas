4844b6b5365b0a976908085eab2381b243d1114a
Leonardo Cunha <cunhacardosoleo@gmail.com>
2021-09-20 15:56:40 -0300

    Array de perguntas movido para StartViewController e inicializado em QuestionViewController. Label com 'número de perguntas' agora dinâmico.

--- perguntasRespostas/Scenes/Questions/QuestionsViewController.swift
+++ perguntasRespostas/Scenes/Questions/QuestionsViewController.swift
@@ -12,23 +12,21 @@ class QuestionsViewController: UIViewController, UITableViewDataSource, UITableV
     
     // MARKS : - Attributes & Variables
     
-    let arrayQuestions: [QuestionModel] = [
-    QuestionModel (question: "1. Qual o nome do presidente do Brasil que ficou conhecido como Jango?", respostas: ["Jânio Quadros", "Jacinto Anjos", "Getúlio Vargas", "João Goulart"], respostaCorreta: 3),
-    QuestionModel (question:"2. Quais os países que têm a maior e a menor expectativa de vida do mundo?", respostas: ["Japão e Serra Leoa", "Austrália e Afeganistão", "Brasil e Congo", "Estados Unidos e Angola"], respostaCorreta: 0),
-    QuestionModel (question: "3. Quanto tempo a luz do Sol demora para chegar à Terra?", respostas: ["12 minutos", "1 dia", "14 horas", "8 minutos"], respostaCorreta: 3),
-    QuestionModel (question: "4. Qual das alternativas menciona apenas símbolos nacionais?", respostas: ["Bandeira insígnia da presidência, bandeira nacional, brasão, hinos e selo", "Bandeira nacional, armas nacionais, hino nacional e selo nacional", "Bandeira nacional, brasão, hino nacional, hino da independência, armas nacionais, cores nacionais, insígnia da presidência, fauna e flora nacional, seleção brasileira de futebol e o pelourinho", "Bandeira insígnia da presidência, brasão flora e fauna e hinos"], respostaCorreta: 1),
-    QuestionModel (question: "5. Qual o maior animal terrestre?", respostas:["Baleia Azul", "Dinossauro", "Elefante Africano", "Tubarão Branco", "Girafa"], respostaCorreta: 2),
-    QuestionModel (question: "6. Qual a religião monoteísta que conta com o maior número de adeptos no mundo?", respostas:["Islamismo", "Cristianismo", "Judaísmo", "Hinduísmo"], respostaCorreta: 1),
-    QuestionModel (question: "7. Qual o nome do cientista que descobriu o processo de pasteurização e a vacina contra a raiva?", respostas:["Marie Curie", "Blaise Pascal", "Louis Pasteur", "Antoine Lavoisier"], respostaCorreta: 2),
-    QuestionModel (question: "8. Quantos graus são necessários para que dois ângulos sejam complementares?", respostas:["45", "90", "180", "360"], respostaCorreta: 1),
-    QuestionModel (question: "9. Qual o metal cujo símbolo químico é o Au?", respostas:["Cobre", "Prata", "Mercúrio", "Ouro"], respostaCorreta: 3),
-    QuestionModel (question: "10. Qual desses filmes foi baseado na obra de Shakespeare?", respostas:["Muito Barulho por Nada (2012)", "Capitães de Areia (2011)", "A Dama das Camélias (1936)", "A Revolução dos Bichos (1954)"], respostaCorreta: 0),
-    ]
+    let arrayQuestions: [QuestionModel]
     
     var questaoAtual: Int = 0
     var indexSelected: IndexPath?
     var score: Int = 0
     var activeButton: Bool = false
+    
+    init(arrayQuestions: [QuestionModel]) {
+        self.arrayQuestions = arrayQuestions
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     // MARKS: - Outlets & Actions
     
